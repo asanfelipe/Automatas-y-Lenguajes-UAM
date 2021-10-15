9152350c504178b6dfdb1e4f4e8c18080e606356
Adri <adrian.sanfelipe@estudiante.uam.es>
2021-10-15 19:13:12 +0200

    3 primeras funciones de re_parser funcionando bien (en teoría)

--- automata/automata/re_parser.py
+++ automata/automata/re_parser.py
@@ -1,7 +1,7 @@
 """Conversion from regex to automata."""
 from automaton import FiniteAutomaton, State, Transition
 from re_parser_interfaces import AbstractREParser
-from typing import Collection
+#from typing import Collection #Esto no hace falta
 
 
 class REParser(AbstractREParser):
@@ -11,13 +11,22 @@ class REParser(AbstractREParser):
         self,
     ) -> FiniteAutomaton:
 
+        #Creamos un estado q0 con el nombre (que es un contador) y diciendole que no es un estado final.
+        #Necesitamos añadir la asignación (name=, is_final=) ya que los argumentos que recibe
+        #FiniteAutomaton son keyword-only
         q0 = State(name=str(self.state_counter), is_final=False)
-
+        #Incrementamos el valor del contador.
         self.state_counter += 1
+        
+        #Hacemos lo mismo con qf
         qf = State(name=str(self.state_counter), is_final=True)
         self.state_counter += 1
+        
+        #Almacenamos en una lista los estados creados anteriormente.
         listaEstados = [q0, qf]
 
+        #Como los argumentos que recibe FiniteAutomaton son keyword-only, añadimos lo comentado anteriormente
+        #de initial_state=, states=, ..., etc
         return FiniteAutomaton(initial_state=q0, states=listaEstados, symbols=[], transitions=[])
 
     def _create_automaton_lambda(
@@ -25,28 +34,32 @@ class REParser(AbstractREParser):
     ) -> FiniteAutomaton:
 
         q0 = State(name=str(self.state_counter), is_final=False)
-
         self.state_counter += 1
-        qf = State(str(self.state_counter), True)
+        
+        qf = State(name=str(self.state_counter), is_final=True)
         self.state_counter += 1
+        
         listaEstados = [q0, qf]
         transiciones = [Transition(q0, None, qf)]
-        return FiniteAutomaton(q0, listaEstados, [], transiciones)
+        
+        return FiniteAutomaton(initial_state=q0, states=listaEstados, symbols=[], transitions=transiciones)
 
     def _create_automaton_symbol(
         self,
         symbol: str,
     ) -> FiniteAutomaton:
 
-        q0 = State(str(self.state_counter), False)
-
+        q0 = State(name=str(self.state_counter), is_final=False)
         self.state_counter += 1
-        qf = State(str(self.state_counter), True)
+        
+        qf = State(name=str(self.state_counter), is_final=True)
+        self.state_counter += 1
 
         listaEstados = [q0, qf]
-        transiciones = Collection[Transition(q0, symbol, qf)]
-        sym = Collection[symbol]
-        return FiniteAutomaton(q0, listaEstados, sym, transiciones)
+        transiciones = [Transition(q0, symbol, qf)]
+        sym = [symbol]
+        
+        return FiniteAutomaton(initial_state=q0, states=listaEstados, symbols=sym, transitions=transiciones)
 
     def _create_automaton_star(
         self,
