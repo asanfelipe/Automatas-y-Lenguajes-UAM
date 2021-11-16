4816031209b2085c2d913e26422f9b4499d2af60
siltom <37139806+siltom@users.noreply.github.com>
2021-11-16 20:21:01 +0100

    Add files via upload

--- automata/automata/automaton.py
+++ automata/automata/automaton.py
@@ -62,10 +62,10 @@ class FiniteAutomaton(
         f_symbols = tuple(set(self.symbols) - {None})
    
         #Inicializamos un set de estados que tendrá el automata determinista vacia
-        f_states = set()
+        f_states = dict()
         #Almacenamos en f_initial_state el primer elemento (un estado) del conjunto f_initial_state_set
         state_name = ""
-        for elem in f_initial_state_set:
+        for elem in sorted(f_initial_state_set):
             state_name += elem.name
         if state_name == "":
             state_name = "empty"
@@ -74,29 +74,19 @@ class FiniteAutomaton(
             if estados.is_final == True:
                 f_initial_state.is_final = True
         #Añadimos a los estados que tendrá el automata determinista el estado anterior
-        
-        f_states.add(f_initial_state)
+        f_states[state_name] = f_initial_state
+ 
         #Inicializamos una lista de transiciones que tendrá el automata determinista vacía
         f_transitions = set()      
         
-        print("Estado inicial:")
-        print(f_initial_state)
-        print("Simbolos:")
-        print(f_symbols)
-
         #Recorremos todos los estados que hemos ido almacenando en el nuevo autómata finito para comprobar todas
-        #las transiciones posibles
-        #for s in f_states:        
-            #print("Estados:")
-            #print(s)
-        #Para cada simbolo en los simbolos del automata
+        #las transiciones posibles para cada simbolo en los simbolos del automata
         f_states_asSet = [f_initial_state_set]
         new_states = [f_initial_state_set]
         evaluated_states = []
         while (new_states):
             for state in new_states:
                 for symbol in f_symbols:
-                    print(symbol)
                     #Llamamos a process_symbol con dicho simbolo
                     evaluator.current_states = state
                     evaluator.process_symbol(symbol)
@@ -110,39 +100,58 @@ class FiniteAutomaton(
                         f_states_asSet.append(result)
                     #Asignamos el estado "result" en la variable final_state
                     state_name = ""
-                    for elem in result:
+                    for elem in sorted(result):
                         state_name += elem.name
                     if state_name == "":
                         state_name = "empty"
-                    final_state = State(state_name)
-                    for estados in result:
-                        if estados.is_final == True:
-                            final_state.is_final = True
-                    print(final_state)
                     
-                    #Añadimos a la lista de estados del automata determinista final_state            
-                    f_states.add(final_state)                    
-                    #Añadimos la transición a la lista de transiciones con los datos recogidos
+                    if state_name in f_states:
+
+                        final_state = f_states[state_name]
+
+                    else:
+
+                        final_state = State(state_name)
+                        for estados in result:
+                            if estados.is_final == True:
+                                final_state.is_final = True
+
+                        f_states[state_name] = final_state
+                    
+                    #Añadimos a la lista de estados del automata determinista initial_state                          
                     state_name = ""
-                    for elem in state:
+                    for elem in sorted(state):
                         state_name += elem.name
                     if state_name == "":
                         state_name = "empty"
                         
-                    initial_state = State(state_name)
-                    for estados in state:
-                        if estados.is_final == True:
-                            initial_state.is_final = True
+                    if state_name in f_states:
+
+                        initial_state = f_states[state_name]
+
+                    else:
+
+                        initial_state = State(state_name)
+                        for estados in state:
+                            if estados.is_final == True:
+                                initial_state.is_final = True
+
+                        f_states[state_name] = initial_state
+
+                    #Añadimos la transición a la lista de transiciones con los datos recogidos
                     transicion = Transition(initial_state, symbol, final_state)
                     f_transitions.add(transicion)
                     
             evaluated_states = evaluated_states + new_states
             new_states = [elem for elem in f_states_asSet if elem not in evaluated_states] 
-            
-        print("Transiciones:")
-        print(f_transitions)   
+
+        #Almacenamos en un set los datos del diccionario
+        states_set = set()
+        for s in f_states.values():
+            states_set.add(s)
+
         #Retornamos el autómata finito con los sets de datos que hemos ido almacenando
-        return FiniteAutomaton(initial_state=f_initial_state, states=f_states, symbols=f_symbols, transitions=f_transitions)
+        return FiniteAutomaton(initial_state=f_initial_state, states=states_set, symbols=f_symbols, transitions=f_transitions)
 
     def to_minimized(
         self,
