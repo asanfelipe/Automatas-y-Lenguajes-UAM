d654958169d7836d0b572c6513385cd9147972a5
Adri <adrian.sanfelipe@estudiante.uam.es>
2021-11-21 09:49:12 +0100

    Ejercicio 1a hecho

--- grammar/ast_utils.py
+++ grammar/ast_utils.py
@@ -1,9 +1,17 @@
 import ast
 import numbers
+
 class ASTMagicNumberDetector(ast.NodeVisitor):
     def __init__(self) -> None:
         self.magic_numbers=0
-    def check_magic_number(self, number:complex) -> None:
+
+    def _check_magic_number(self, number:complex) -> None:
         if isinstance(number, numbers.Number):
-            self.magic_numbers = self.magic_numbers + 1
-    #def visit_Num(self, node:ast.Num) -> None:
+            if number != 0 | number != 1 | number != 1j:
+                self.magic_numbers = self.magic_numbers + 1
+
+    def visit_Num(self, node:ast.Num) -> None:
+        self._check_magic_number(node.n)
+
+    def visit_Constant(self, node: ast.Constant) -> None:
+        self._check_magic_number(node.value)
