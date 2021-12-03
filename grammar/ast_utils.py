dfb1b3e903279687911f28d20dc284a11a650514
siltom <37139806+siltom@users.noreply.github.com>
2021-12-03 10:52:50 +0100

    Update ast_utils.py

--- grammar/ast_utils.py
+++ grammar/ast_utils.py
@@ -22,25 +22,31 @@ class ASTMagicNumberDetector(ast.NodeVisitor):
 class ASTDotVisitor(ast.NodeVisitor):
 
     def __init__(self) -> None:
-        self.level = 0
+        self.root = 0
         self.n_node = 0
         self.last_parent: Optional[int] = None
         self.last_field_name = ""
     
     def generic_visit(self, node: ast.AST) -> None:
+        if self.root == 0:
+            print("digraph {")
         for field, value in ast.iter_fields(node):
+            self.root = 1
             if isinstance(value, list):
                 for item in value:
-                    if isinstance(item, AST):
+                    if isinstance(item, type(ast)):
                         print(field)
                         print(item)
                         item.padre = node ########no estoy segura de esto#######
                         self.visit(item)
-            elif isinstance(value, AST):
+            elif isinstance(value, ast):
                 print(field)
                 print(value)
                 value.padre = node ########no estoy segura de esto#######
                 self.visit(value)
+        self.root = 0
+        if self.root == 0:
+            print("}")
 
 # usar un esquema similar al generic_visit de la clase padre para recorrer los hijos del nodo actual
 # obtener los nodos hijos (nodos AST y listas de nodos AST) del nodo actual
